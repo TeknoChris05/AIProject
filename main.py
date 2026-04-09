4412133cf28b5a6d0e1d54b933ea90b2daf7691f
TeknoChris05 <christopher.gatie@gmail.com>
2026-04-09 09:58:59 -0400

    Main.py update

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.pipeline import run_pipeline
-from src.reporting import to_markdown
-
-if __name__ == "__main__":
-    path = "src/data/samples/auth_sample.log"
-
-    report = run_pipeline(path)
-    print(to_markdown(report))
